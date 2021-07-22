983e3c8cd425ef7b1202f282e19babd596393c14
chenglin.xie <hAH.6PJyhWl_>
2021-07-22 17:31:58 +0800

    echo

---
+++ bash/echo.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+echo "hello world"
+
+name=helloworld
+
+#显示转义字符
+echo "\"$name"
+
+echo -e "OK! \n  dd" # -e 开启转义
+
+#显示命令结果
+echo `date`
+
+echo 'hello world'>'C:\Users\EDZ\Desktop\d'
